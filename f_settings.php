d64d5a62bd6812304b79006776ee5324ecbc3a9d
Dustin-Desktop <dustin@dustinaguilar.com>
2017-08-28 23:48:49 -0400

    First Push for Citywide Appliance Repair
    This is the third site for my appliance repair client. This version is
    mobile friendly, among other things. I'll be applying this template to
    the others.

---
+++ f_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "kim.brinkman@att.net"; // your email address
+$email_subject = "Repair Inquiry from Website"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+?>
